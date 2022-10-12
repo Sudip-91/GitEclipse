1e7d01f438a744632f6080d83341356de26f560c
Binita143Bajracharya <bbajrachary@student.holmesglen.edu.au>
2022-10-12 17:22:52 +1100

    commit-3

--- Demo1/src/GitDemo/Hello.java
+++ Demo1/src/GitDemo/Hello.java
@@ -4,7 +4,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Line number 1");
-		System.out.println("Line number 1");
+		System.out.println("Line number 2");
+		System.out.println("Line number 3");
 
 	}
 
